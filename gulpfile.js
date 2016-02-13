6737cc48b38649ac31a55438e6771a5314f25e2b
Stereokai <Email me at tom@ (my website url)>
2016-02-13 11:49:52 +0200

    Latest version

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,15 @@
-var gulp = require('gulp'),
-    sass = require('gulp-sass'),
-    browserSync = require('browser-sync'),
+var gulp         = require('gulp'),
+    sass         = require('gulp-sass'),
+    bulkSass     = require('gulp-sass-bulk-import'),
+    filelog      = require('gulp-filelog'),
+    browserSync  = require('browser-sync'),
     autoprefixer = require('gulp-autoprefixer'),
-    uglify = require('gulp-uglify'),
-    jshint = require('gulp-jshint'),
-    header  = require('gulp-header'),
-    rename = require('gulp-rename'),
-    minifyCSS = require('gulp-minify-css'),
-    package = require('./package.json');
+    uglify       = require('gulp-uglify'),
+    jshint       = require('gulp-jshint'),
+    header       = require('gulp-header'),
+    rename       = require('gulp-rename'),
+    minifyCSS    = require('gulp-minify-css'),
+    package      = require('./package.json');
 
 
 var banner = [
@@ -24,6 +26,7 @@ var banner = [
 
 gulp.task('css', function () {
   return gulp.src('src/scss/style.scss')
+  .pipe(bulkSass())
   .pipe(sass({errLogToConsole: true}))
   .pipe(autoprefixer('last 2 version'))
   .pipe(gulp.dest('app/assets/css'))
@@ -61,7 +64,7 @@ gulp.task('bs-reload', function () {
 });
 
 gulp.task('default', ['css', 'js', 'browser-sync'], function () {
-  gulp.watch("src/scss/*/*.scss", ['css']);
+  gulp.watch(["src/scss/*/*.scss", "src/scss/modules/*"], ['css']);
   gulp.watch("src/js/*.js", ['js']);
   gulp.watch("app/*.html", ['bs-reload']);
 });
